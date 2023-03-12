1cf014d2f81d66a77058c4e252ad390f23b5b521
oswaldo dev <devweb@oswaldo.dev>
2023-03-12 20:29:42 +0100

    Primer ejercicio de javaScript

---
+++ Ejercicio001.js
@@ -0,0 +1,14 @@
+//Variables let y var
+
+//var no da error
+var valor1  ="valor 1";
+var valor1 = "valor 2"
+
+console.log(valor1);
+
+
+// Da error con let
+let valor = "oswaldo";
+let valor = "oswaldo";
+
+console.log(valor);
